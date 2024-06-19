d80d4aa01e7a0b6520e3e92d2f8499d5841c2d5b
Thiago Cruz Machado <96290053+yTrosky@users.noreply.github.com>
2024-06-19 10:35:40 -0300

    Update banco.sql

--- database/banco.sql
+++ database/banco.sql
@@ -2,19 +2,24 @@
 CREATE DATABASE IF NOT EXISTS apiusers;
 USE apiusers;
 
--- Criação da tabela users
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL
-);
+-- Copiando estrutura para tabela apiusers.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT '0',
+  `email` varchar(150) NOT NULL DEFAULT '0',
+  `password` varchar(200) NOT NULL DEFAULT '0',
+  `role` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
--- Criação da tabela passwordtokens
-CREATE TABLE IF NOT EXISTS passwordtokens (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    token VARCHAR(255) NOT NULL UNIQUE,
-    user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
+-- Copiando estrutura para tabela apiusers.passwordtokens
+CREATE TABLE IF NOT EXISTS `passwordtokens` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `token` varchar(200) NOT NULL DEFAULT '0',
+  `user_id` int(10) unsigned NOT NULL DEFAULT 0,
+  `used` tinyint(3) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `FK_passwordtokens_users` (`user_id`),
+  CONSTRAINT `FK_passwordtokens_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
